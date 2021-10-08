2cf587f011c296b65594ea566fee8c80ab1b6b23
1binibin <namwonbin@gmail.com>
2021-10-08 16:29:18 +0900

    Daily Component 작업 전

--- src/api/api-daily.js
+++ src/api/api-daily.js
@@ -3,9 +3,10 @@ import axios from 'axios'
 const appid = '170deeb7b5aa46780d060e302f27ad70'
 const url = 'https://api.openweathermap.org/data/2.5/weather'
 // const icons = ['https://openweathermap.org/img/wn/', '@2x.png']
-const params = { units: 'metric', appid }
+const params = { units: 'metric', lang: 'kr', appid }
 
 const apiDaily = options => {
+  params.lang = options.lang || 'kr'
   if (options.id) {
     params.id = options.id
   } else if (options.lat && options.lon) {
