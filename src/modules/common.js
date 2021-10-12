6801e4d4cd467fad34716e5a2a9202b07bb0b319
1binibin <namwonbin@gmail.com>
2021-10-12 15:01:41 +0900

    GET_CITY 작업중

--- src/modules/common.js
+++ src/modules/common.js
@@ -1,5 +1,10 @@
 const PLACEHOLDER = 'http://via.placeholder.com/100/FFFFFF?text='
+const DEF_COORDS = {
+  lat: 37.56826,
+  lon: 126.977829
+}
 
 export {
-  PLACEHOLDER
+  PLACEHOLDER,
+  DEF_COORDS
 }
